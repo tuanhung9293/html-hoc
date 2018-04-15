168f4a2b9060a053b7a136a765e1b589bc091732
Tuan Ho <tho@openwt.com>
2018-04-15 18:54:47 +0700

    push

--- js/script.js
+++ js/script.js
@@ -5,4 +5,39 @@ $(document).ready(function () {
         $(selector).removeClass('active');
         $(this).addClass('active');
     });
+
+    var carousel = $('#carousel');
+    $("#carousel .slide").clone().appendTo("#carousel");
+    
+    $('#next').click(function () { shiftSlide(-1) })
+    $('#prev').click(function () { shiftSlide(1) })
+
+    function shiftSlide(direction) {
+        if (direction === 1) {
+            var slideWidth = $('.slide:last').width();
+
+            $(document).off('mouseup')
+            carousel.off('mousemove')
+                .addClass('transition')
+                .css('transform', 'translateX(' + (slideWidth) + 'px)');
+            setTimeout(function () {
+                $('.slide:first').before($('.slide:last'));
+                carousel.removeClass('transition')
+                carousel.css('transform', 'translateX(0px)');
+            }, 700)
+
+        } else if (direction === -1) {
+            var slideWidth = $('.slide:first').width();
+
+            $(document).off('mouseup')
+            carousel.off('mousemove')
+                .addClass('transition')
+                .css('transform', 'translateX(' + (-slideWidth) + 'px)');
+            setTimeout(function () {
+                $('.slide:last').after($('.slide:first'));
+                carousel.removeClass('transition')
+                carousel.css('transform', 'translateX(0px)');
+            }, 700)
+        }
+    }
 });
